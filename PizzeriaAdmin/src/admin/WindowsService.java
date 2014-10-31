1a38f49dd2d402889a1d028d6cd913480398786a
pizzagato <pizzagato@gmail.com>
2014-10-31 13:49:59 +0200

    Finaaliversio 31.10 katselmukseen

--- PizzeriaAdmin/src/admin/WindowsService.java
+++ PizzeriaAdmin/src/admin/WindowsService.java
@@ -1,27 +1,46 @@
 package admin;
 
-import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.ArrayList;
 
 import javax.swing.Box;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class WindowsService {
-	private final static Component frame = null;
-
+	JFrame frmOpt;  //dummy JFrame
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	double dWidth = screenSize.getWidth();
+	double dHeight = screenSize.getHeight();
+	int width = (int) dWidth/2;
+	int height = (int) dHeight/2;
+	
+	private JFrame alwaysOnTop() {
+		if (frmOpt == null) {
+	        frmOpt = new JFrame();
+	    }
+	    frmOpt.setVisible(true);
+	    frmOpt.setLocation(width, height);
+	    frmOpt.setAlwaysOnTop(true);
+	    return frmOpt;
+	}
+	
 	/*
 	 * Ilmoitusruutu, kun k‰ytt‰j‰ on syˆtt‰nyt kirjautumistiedot liian monta kertaa v‰‰rin
 	 */
 	
 	public void bannedWindow() {
+		frmOpt = alwaysOnTop();
 		final String content = "Virheellisten kirjautumisten m‰‰r‰ t‰ynn‰.\n You have been banned.";
 		final String topic = "Too bad, so sad.";
-		JOptionPane.showMessageDialog(frame,
+		JOptionPane.showMessageDialog(frmOpt,
 			    content, topic,
 			    JOptionPane.WARNING_MESSAGE);
+		frmOpt.dispose();
 	}
 	
 	/*
@@ -29,12 +48,14 @@ public class WindowsService {
 	 */
 	
 	public void isBannedWindow(int timeLeft) {
+		frmOpt = alwaysOnTop();
 		final String content = "P‰‰sy estetty.\n Voit koittaa uudelleen\n" + 
 			    timeLeft + " minuutin kuluttua.";
 		final String topic = "Too bad, so sad.";
-		JOptionPane.showMessageDialog(frame,
+		JOptionPane.showMessageDialog(frmOpt,
 			    content, topic,
 			    JOptionPane.WARNING_MESSAGE);
+		frmOpt.dispose();
 	}
 	
 	/*
@@ -43,6 +64,7 @@ public class WindowsService {
 	
 	@SuppressWarnings("deprecation")
 	public ArrayList<String> loginWindow() {
+		frmOpt = alwaysOnTop();
 		final String topic = "Kirjautuminen";
 		ArrayList<String> idPassword = new ArrayList<String>();
 		JTextField jtf = new JTextField(16);
@@ -55,7 +77,8 @@ public class WindowsService {
 	    box.add(jtf);
 	    box.add(ls);
 	    box.add(jpf);
-	    int x = JOptionPane.showConfirmDialog(null, box, topic, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+	    int x = JOptionPane.showConfirmDialog(frmOpt, box, topic, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+	    frmOpt.dispose();
 		if (x == JOptionPane.CANCEL_OPTION || x == JOptionPane.CLOSED_OPTION) {
 			return idPassword;
 		}
