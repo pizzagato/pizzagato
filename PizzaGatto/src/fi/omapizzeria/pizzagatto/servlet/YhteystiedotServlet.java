92bcc49e38d9b38a697bfa48903d2ed57e2519b4
a1303433 <a1303433@pao5008w26.haagahelia.amk>
2014-11-26 12:49:26 +0200

    JavaDoc

--- PizzaGatto/src/fi/omapizzeria/pizzagatto/servlet/YhteystiedotServlet.java
+++ PizzaGatto/src/fi/omapizzeria/pizzagatto/servlet/YhteystiedotServlet.java
@@ -8,24 +8,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * Tämä servletti toimii kontrollerina yhteystietosivun näyttämiseen.
+ */
 @WebServlet("/Yhteystiedot") //Pääte joka näkyy URLissa
 public class YhteystiedotServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public YhteystiedotServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-    //Ohjaa yhteystietosivulle
+	/**
+	 * @param request Selaimelta tuleva pyyntö
+	 * @param reponse Selaimelle lähetettävä vastaus
+	 * 
+	 * Tämä metodi ohjaa suoraan yhteystietosivulle
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		request.getRequestDispatcher("WEB-INF/jsp/yhteystiedot.html").forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
